6fc731dc4641023fbb672102a0096eab0149c1a0
Mandi Grant <mandi018@gmail.com>
2014-07-22 22:12:08 -0700

    Porting over from class folder. This update includes movie posters, the out-of-order passcode entry fix, the addition of variable passcodes, and many minor visual improvements.

--- scripts.js
+++ scripts.js
@@ -4,30 +4,35 @@ $(document).ready(function () {
 	$("#ep1").show();
 	var episodeNum = 1;
 
+	var passcode = $("#ep1").data("passcode");
+	$(".printPasscode").html(passcode);
+
 	//attach the passcode prompt to each plot
 	$('.passcodePrompt').insertAfter($(".plot"));
 
 	//tab seletion and content showing/hiding logic
 	$(".tab").click(function() {
+		episodeNum = $(this).val();
+		passcode = $("#ep"+episodeNum).data("passcode");
 		//highlight existing tab, unhighlight the rest
 		$(".tab").removeClass('tabSelected');
 		$(this).addClass('tabSelected');
 		
 		$(".contents").hide(); //hide all the contents divs
-		episodeNum = $(this).val();
 		$("#ep" + episodeNum).show(); //show just the selected film contents
+		$("#ep" + episodeNum).find(".printPasscode").html(passcode);
 	});
 
 	//accept user passcode and reveal selected film's ending
 	$(".reveal").click(function() {
-		var input = $("input").val().toLowerCase();
-		if (input == "jedi") {
-			$("#ep" + episodeNum).find(".ending").show(1000);
-			$("#ep" + episodeNum).find(".passcodePrompt").hide(1000);
+		var input = $("#ep"+episodeNum).find(".passcodeInput").val();
+		if (input.toLowerCase() == passcode.toLowerCase()) {
+			$("#ep" + episodeNum).find(".ending").show();
+			$("#ep" + episodeNum).find(".passcodePrompt").hide();
 		} else {
+			alert(input);
 			$(".error").html("");
 			$(".error").append("Enter the passcode.");
 		}
 	});
-
 });
